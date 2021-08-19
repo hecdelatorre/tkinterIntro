99b458b2f9c4b75eed4a5b869f91abb0a7c87c12
Hec98 <hector982015@gmail.com>
2021-08-18 23:05:04 -0500

    Label

--- intro.py
+++ intro.py
@@ -4,9 +4,11 @@ root = Tk()
 root.title('Hello World')
 root.geometry('500x300')
 
-Label(root, text = 'Hello, this is a tag').pack()
-Label(root, text = 'Hello, this is a tag').pack()
-Label(root, text = 'Hello, this is a tag').pack()
+lb1 = Label(root, text = 'Hello, this is a tag')
+lb2 = Label(root, text = 'Hello, this is a tag')
+# Label(root, text = 'Hello, this is a tag').pack()
 
+lb1.grid(row=0, column=0)
+lb2.grid(row=0, column=1)
 
 root.mainloop()
