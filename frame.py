d44f32ee08fb6a79db06afd7594d18d199369fad
Hec98 <hector982015@gmail.com>
2021-08-20 01:12:38 -0500

    Frame

--- frame.py
+++ frame.py
@@ -1,10 +1,11 @@
-from tkinter import Tk, LabelFrame, Label, Button
+from tkinter import Tk, LabelFrame, Frame, Label, Button
 
 root = Tk()
 root.title('Frame')
 
 # frame = LabelFrame(root, text='Login', padx=10, pady=10, borderwidth=3)
-frame = LabelFrame(root, padx=20, pady=10, borderwidth=3)
+# frame = LabelFrame(root, padx=20, pady=10, borderwidth=3)
+frame = Frame(root, padx=20, pady=10)
 frame.pack(padx=20, pady=20)
 
 lb = Label(frame, text="I'm in a frame")
